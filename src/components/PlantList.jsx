598cd780c41550a9965810ae71d4ef89e93bf9ca
Jamie Bryson <jamieab98@gmail.com>
2025-11-04 17:23:21 -0500

    Did some work. Got to the point where we are bringing in creating one PlantCard component every plant (object) in the backend ;)

--- src/components/PlantList.jsx
+++ src/components/PlantList.jsx
@@ -1,9 +1,33 @@
 import React from "react";
 import PlantCard from "./PlantCard";
+import { useState, useEffect } from "react";
 
 function PlantList() {
+
+  const [plants, setPlants] = useState([])
+
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+    .then(response => {
+      if (!response.ok) {console.log("failed to fetch")}
+      return response.json();
+    })
+    .then(plants => {
+      setPlants(plants)
+      console.log(plants)
+    })
+    .catch(error => console.log(error));
+  }, [])
+
+
+
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <ul className="cards">{/* render PlantCards components in here */}
+      {plants.map((p) => (
+        <PlantCard />
+      )) }
+    </ul>
   );
 }
 
